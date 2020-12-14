8636f1793a206e59f9d33e7446fdd4771ef3e9d5
Patrick Latter <palatter@microsoft.com>
2020-12-14 12:39:18 -0800

    Initial project commit

---
+++ MeetingSDKGettingStarted/MeetingSDKGettingStarted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetingSDKGettingStarted
+//
+//  Created by Patrick Latter on 2020-12-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
